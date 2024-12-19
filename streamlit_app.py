de203507b20ed51a9c34a05e2700ef7642536e19
fundacionoli <alianzas@olifoundation.org>
2024-12-18 19:42:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="Buscador de ONGs", page_icon="🌍", layout="wide
 # Función para cargar datos desde Google Sheets
 @st.cache_data
 def load_data():
-    sheet_id = "1mrCQubiQSk_Ilv9a5gbezH_zyu81DjCr_G1sBHZ93Lg"
+    sheet_id = "123_uZ-5G5sNaq9lFNFhVYOcGE7jRww_bUnjs72dDEdM"
     sheet_name = "Respuestas de formulario 1"
     url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=xlsx"
     df = pd.read_excel(url, sheet_name=sheet_name, engine='openpyxl')
